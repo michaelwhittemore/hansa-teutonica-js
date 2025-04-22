bf7171e402a72f35c185b395a318beb6f509c9dc
Michael Whittemore <michaelwhittemore@gmail.com>
2025-04-22 15:45:46 -0700

    landing page now encodes query parameters, still need to load them in the main page

--- public/src/landingPage.js
+++ public/src/landingPage.js
@@ -2,9 +2,7 @@ import { TEST_PLAYERS } from "./helpers/constants.js"
 import { createDivWithClassAndIdAndStyle } from "./helpers/helpers.js";
 
 const populatePlayerSelectionWithDefault = () => {
-    // Populate with default using TEST_PLAYERS
     for (let i = 0; i < TEST_PLAYERS.length; i++) {
-        console.log(TEST_PLAYERS[i])
         // I'm not zero indexing for UI
         document.getElementById(`playerName-${i+1}`).value = TEST_PLAYERS[i][0]
         document.getElementById(`playerColor-${i+1}`).value = TEST_PLAYERS[i][1]
@@ -14,13 +12,11 @@ const populatePlayerSelectionWithDefault = () => {
 
 const populatePlayerSelection = (playerNumber) => {
     const playerSelector = document.getElementById('playerSelector')
-    // need a helper to add the element
     // need to check the current number of elements - not sure if can just get direct children
 
-    for (let i = 1; i <= playerNumber; i++) {
-        playerSelector.append(createPlayerInfoDiv(i))
+    for (let i = 0; i < playerNumber; i++) {
+        playerSelector.append(createPlayerInfoDiv(i + 1))
     }
-
 };
 
 const createPlayerInfoDiv = (id) => {
@@ -41,11 +37,49 @@ const createPlayerInfoDiv = (id) => {
     return playerInfoDiv
 }
 
-const bindButtons = () => {
-    const playerNumberDropdown = document.getElementById('playerNumber')
+const startGame = () => {
+    // TODO
+    // Need to find gamemode, but right now I'm, going to default to hotseat
+    // Will need some validation here, but let's not worry about it for the moment
+    // We can't allow special characters because they would screw up my URL I think
+    // I don't need the player array, can directly add to URL
+    // May need to encodeURI for special charcters
+    const url = new URL(document.location.href);
+    url.pathname = 'hotseat'
+
+    const playerSelector = document.getElementById('playerSelector')
+    for (let i = 0; i < playerSelector.childElementCount; i++ ){
+        const id = i + 1;
+        const name = document.getElementById(`playerName-${id}`).value;
+        const color = document.getElementById(`playerColor-${id}`).value;
+
+        url.searchParams.append(`playerName-${id}`, name)
+        url.searchParams.append(`playerColor-${id}`, color)
+    }
+
+    console.log(url)
+    if (!URL.canParse(url)){
+        console.error('Can not parse url', url)
+    }
+    window.location.assign(url)
+
 }
 
-// TODO's eventually we would like the config to pop up when you select "hotseat" - possibly just hide it otherwise
+const bindButtons = () => {
+    const playerNumberDropdown = document.getElementById('playerNumber')
+    document.getElementById('start').onclick = startGame
+}
+
+// TODO's 
+/* 
+* Eventually we would like the config to pop up when you select "hotseat" - possibly just hide it otherwise
+* Need to validate colors work
+* Need to sanitize player input - both kinds
+* Use a nicer color selector
+* Add an onchange to the player number drop down
+* Add the start button with the url parser
+* Start should also be hidden until the game mode is selected
+**/ 
 
 const start = () => {
     populatePlayerSelection(4)
