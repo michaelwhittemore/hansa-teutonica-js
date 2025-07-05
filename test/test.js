9ab2ada9d1d8ff1f9d1733303e0480281edd3f65
Michael Whittemore <michaelwhittemore@gmail.com>
2025-07-05 13:14:44 -0700

    going to try to create an inner board wrapper for scoll to look nicer

--- test/test.js
+++ test/test.js
@@ -24,6 +24,8 @@ const start = () => {
     const city2 = createDivWithClassAndIdAndStyle(['testCity'], 'city2', { backgroundColor: 'yellow' })
     const city3 = createDivWithClassAndIdAndStyle(['testCity'], 'city3')
     const city4 = createDivWithClassAndIdAndStyle(['testCity'], 'city4')
+    const city5 = createDivWithClassAndIdAndStyle(['testCity'], 'city5')
+
 
     // We want to test that if the element is outside the view it's scrollable
     // Will need to use the overflow tag
@@ -32,13 +34,17 @@ const start = () => {
     // here! - my next step is to create a method to take in the coordinates and apply that to
     // a transform function - I should also offset the testWrapper to simulate what actually will happen
 
-
-    testWrapper.append(city1, city2, city3, city4)
+    // it's not an issue with scroll, it's an issue with my function - I think it doesn't play nice with a pre-transofrm
+    // ok that explains city3, but what about city 5?? - oh I'm not appending it
+    testWrapper.append(city1, city2, city3, city4, city5)
     console.log(city1.getBoundingClientRect())
     console.log(city2.getBoundingClientRect())
     translateElement(city1, 200, 200)
     translateElement(city2, 210, 210)
-
+    translateElement(city3, 475, 45)
+    translateElement(city4, 13, 600)
+    translateElement(city5, 470, 15)
+    // I have an idea! lets create the board WITHIN the wrapper
 
     // let's try with 200, 200.
 }
